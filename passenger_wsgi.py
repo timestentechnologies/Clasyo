fb288775cac8e6ede89f3c8c03e8c67e34227883
alex.mwangi <alex.mwangi@medserv-africa.com>
2025-11-06 17:02:05 +0300

    psg

---
+++ passenger_wsgi.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+# Add your project directory to the sys.path
+project_home = os.path.dirname(os.path.abspath(__file__))
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Set environment variables
+os.environ['DJANGO_SETTINGS_MODULE'] = 'schoolsaas.settings'
+
+# Import Django application
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
