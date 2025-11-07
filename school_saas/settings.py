51fe910ffee5f59affed18b8d96de3e749500419
alex.mwangi <alex.mwangi@medserv-africa.com>
2025-11-07 10:07:04 +0300

    updates

--- school_saas/settings.py
+++ school_saas/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-key-change-in-product
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = [host.strip() for host in config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')]
 
 # Application definition
 INSTALLED_APPS = [
